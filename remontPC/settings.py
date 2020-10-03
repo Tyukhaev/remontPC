d32c01353bb3299f18bf52913af73e7a306abb53
Ivan Tyukhay <faaaaa88471@gmail.com>
2020-10-03 14:48:54 +0800

    last try

--- remontPC/settings.py
+++ remontPC/settings.py
@@ -136,6 +136,7 @@ DATABASES['default'].update(db_from_env)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
